4cbf78c5fda958c95a59197752f837311d03a04a
dima.avdeev <dima.avdeev@jetbrains.com>
2023-02-19 13:40:35 +0400

    sample

--- compose/mpp/demo-uikit/src/uikitMain/kotlin/androidx/compose/mpp/demo/getViewControllerWithCompose.kt
+++ compose/mpp/demo-uikit/src/uikitMain/kotlin/androidx/compose/mpp/demo/getViewControllerWithCompose.kt
@@ -16,47 +16,42 @@
 
 package androidx.compose.mpp.demo
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.*
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Application
 
-fun getViewControllerWithCompose() = Application("Compose/Native sample") {
-    val textState1 = remember { mutableStateOf("text field 1") }
-    val textState2 = remember { mutableStateOf("text field 2") }
-    Column {
-        Text(".")
-        Text(".")
-        Text(".")
-        Text(".")
-        Text(".")
-        Text(".")
-        Text(".")
-        Text(".")
-        Text("Hello, UIKit")
-        TextField(value = textState1.value, onValueChange = {
-            textState1.value = it
-        })
-        TextField(value = textState2.value, onValueChange = {
-            textState2.value = it
-        })
-        Image(
-            painter = object : Painter() {
-                override val intrinsicSize: Size = Size(16f, 16f)
-                override fun DrawScope.onDraw() {
-                    drawRect(color = Color.Blue)
-                }
-            },
-            contentDescription = "image sample"
-        )
+fun getViewControllerWithCompose() =
+    Application("Compose/Native sample") {
+        val t = remember { mutableStateOf("text state") }
+        Box(Modifier.fillMaxSize()) {
+            Text(buildString { repeat(5){appendLine("Up $it")} }, Modifier.align(Alignment.TopStart).background(Color.LightGray))
+            TextField(t.value, {t.value = it}, Modifier.align(Alignment.Center).fillMaxWidth())
+            Text(buildString { repeat(5){appendLine("Down $it")} }, Modifier.align(Alignment.BottomStart).background(Color.LightGray))
+        }
     }
-}
